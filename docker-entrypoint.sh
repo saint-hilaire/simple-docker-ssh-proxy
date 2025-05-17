615a089257f5ac9ce07a0b6624f8d984af6f8a62
Brian St. Hilaire <brian.st-hilaire@sanctus-tech.com>
2025-05-18 00:47:40 +0200

    (continue) feature/gh-1-alpine
    - Hope this works, thx Copilot ^^

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ssh-keygen -A
+# Remove conflicting AuthorizedKeysFile line
+sed -i '/AuthorizedKeysFile/d' /etc/ssh/sshd_config
 exec /usr/sbin/sshd -D -e -o AuthorizedKeysFile=/home/proxy_user/.ssh/authorized_keys "$@"
