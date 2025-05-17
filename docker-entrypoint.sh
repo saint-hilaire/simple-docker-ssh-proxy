e3d8e9a579c447de55ec5a57668042b10868405b
Brian St. Hilaire <brian.st-hilaire@sanctus-tech.com>
2025-05-18 01:33:55 +0200

    continue: NOT TESTED!

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,7 @@ ssh-keygen -A
 # Remove conflicting AuthorizedKeysFile line
 #sed -i '/AuthorizedKeysFile/d' /etc/ssh/sshd_config
 exec /usr/sbin/sshd -D -e \
-  -o "AuthorizedKeysFile=/home/proxy_user/.ssh/authorized_keys,AllowTcpForwarding=yes" \
+  -o AuthorizedKeysFile=/home/proxy_user/.ssh/authorized_keys \
+  -o AllowTcpForwarding=yes \
+  -o PasswordAuthentication=no \
   "$@"
